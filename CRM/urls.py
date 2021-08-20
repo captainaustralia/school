83ce2625ce4205ea02b2bc13b63b3a65602d4408
mwmwm <61281668+physrow@users.noreply.github.com>
2021-08-20 09:35:24 +0700

    update List/Detail view

--- CRM/urls.py
+++ CRM/urls.py
@@ -3,5 +3,10 @@ from django.urls import path, include
 from main.views import index, to_crm
 from personalarea.views import lk
 
+from django.conf.urls import url
+from CRM import views
+
 urlpatterns = [
+    path('<int:pk>/', views.StudentDetail.as_view(), name='CRM'),
+    path('students/', views.StudentList.as_view(), name='std_list'),
 ]
